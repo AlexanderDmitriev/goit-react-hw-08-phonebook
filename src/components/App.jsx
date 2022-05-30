37b01b0b86506ffca8189b4c8c88f009dc167c75
AlexanderDmitriev <alex9110077@gmail.com>
2022-05-30 23:50:02 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { lazy, Suspense } from 'react';
 import StartPage from '../pages/StartPage';
 import ContactsPage from '../pages/ContactsPage';
-import RegisterPage from '../pages/RegisterPage';
 import LoginPage from '../pages/LoginPage';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { dark, light } from '../theme';
-import { useState, useEffect } from 'react';
-import { lazy, Suspense } from 'react';
+import { useDispatch } from 'react-redux';
+import { useAddNewUserMutation } from '../redux/user';
 
 const Navigation = lazy(() => import('./Navigation'));
+const RegisterPage = lazy(() => import('../pages/RegisterPage'));
 
 export const App = () => {
+  ///Theming
   const [isDarkTheme, setIsDarkTheme] = useState(
     JSON.parse(window.localStorage.getItem('darkTheme')) ?? false
   );
@@ -23,6 +26,14 @@ export const App = () => {
     window.localStorage.setItem('darkTheme', JSON.stringify(isDarkTheme));
   }, [isDarkTheme]);
 
+  ///Create user
+  const dispatch = useDispatch();
+  const [createNewUser /* isSuccess */] = useAddNewUserMutation();
+
+  const registrationUser = ({ name, email, password }) => {
+    dispatch(createNewUser({ name, email, password }));
+  };
+
   return (
     <ThemeProvider theme={isDarkTheme ? createTheme(dark) : createTheme(light)}>
       <Suspense fallback={<div>Loading...</div>}>
@@ -31,7 +42,7 @@ export const App = () => {
           <Route path="/" element={<StartPage />} />
           <Route path="/contacts" element={<ContactsPage />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/register" element={<RegisterPage onSubmit={registrationUser}/>} />
         </Routes>
         <h2>NewContactModal</h2>
       </Suspense>
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -4,8 +4,10 @@ import { useAddNewUserMutation } from '../redux/user';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
+import { Formik, Form, ErrorMessage, Field } from 'formik';
+import * as yup from 'yup';
 
-const RegisterPage = () => {
+const RegisterPage = ({ onSubmit }) => {
   const registrationMessage = `Please, fill out the registration form to use the application.`;
 
   const dispatch = useDispatch();
@@ -17,60 +19,112 @@ const RegisterPage = () => {
   const handleSubmit = event => {
     event.preventDefault();
     dispatch(createNewUser({ name, email, password }));
-    console.log(event.target);
+    console.log(event);
     setName('');
     setEmail('');
     setPassword('');
   };
 
+  const handleSubmit2 = (values , { resetForm }) => {
+    console.log(values);
+    onSubmit(values);
+    resetForm();
+  };
+
+  //Схема валидации
+  const schema = yup.object().shape({
+    name: yup.string().required(),
+    email: yup.string().email().required(),
+    password: yup.string().required(),
+  });
+
   return (
     <>
       <h2>{registrationMessage}</h2>
-      {/* <form onSubmit={handleSubmit}> */}
-        <Box
-          component="form"
-          sx={{
-            '& > :not(style)': { m: 1, width: '25ch' },
-          }}
-          noValidate
-          onSubmit={handleSubmit}
-        >
-          <TextField
-            id="name"
-            label="Enter your name"
-            variant="standard"
-            onChange={setName}
-          />
-          <TextField
-            id="email"
-            label="Enter your email"
-            variant="standard"
-            onChange={setEmail}
-          />
-          <TextField
-            id="password"
-            label="Enter your password"
-            variant="standard"
-            onChange={setPassword}
-          />
-        
-        {/* <label>
-          Name
-          <input type="text" name="name" onChange={setName}/>
-        </label>
-        <label>
-          Email
-          <input type="email" name="email" onChange={setEmail}/>
-        </label>
-        <label>
-          Password
-          <input type="text" name="password" onChange={setPassword}/>
-        </label> */}
-        <Button variant="contained" size="medium" type='submit'>
+
+      {/* Пробую через MUI */}
+      <Box
+        component="form"
+        sx={{
+          '& > :not(style)': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        onSubmit={handleSubmit}
+      >
+        <TextField
+          id="name"
+          label="Enter your name"
+          variant="standard"
+          onChange={setName}
+        />
+        <TextField
+          id="email"
+          label="Enter your email"
+          variant="standard"
+          onChange={setEmail}
+        />
+        <TextField
+          id="password"
+          label="Enter your password"
+          variant="standard"
+          onChange={setPassword}
+        />
+        <Button variant="contained" size="medium" type="submit">
           Submit
         </Button>
-        </Box>
-     {/*  </form> */}
+      </Box>
+
+        {/* Пробую через Formik */}
+      <Formik
+        initialValues={{ name: '', email: '', password: '' }}
+        validationSchema={schema}
+        onSubmit={handleSubmit2}
+      >
+        {({ handleSubmit2, isSubmitting }) => (
+          <div>
+            <Form onSubmit={handleSubmit2}>
+              <label htmlFor="name">
+                Name
+                <Field
+                  type="text"
+                  name="name"
+                  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer"
+                />
+                <ErrorMessage name="name" render={<p>{'Incorrect name'}</p>} />
+              </label>
+              <label htmlFor="number">
+                Number
+                <Field
+                  type="email"
+                  name="email"
+                  title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                  required
+                />
+                <ErrorMessage
+                  name="email"
+                  render={msg => <p>{'Incorrect number'}</p>}
+                />
+              </label>
+              <label htmlFor="password">
+                Number
+                <Field
+                  type="text"
+                  name="password"
+                  title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                  required
+                />
+                <ErrorMessage
+                  name="password"
+                  render={msg => <p>{'Incorrect number'}</p>}
+                />
+              </label>
+              <button type="submit" disabled={isSubmitting}>
+                Add contact
+              </button>
+            </Form>
+          </div>
+        )}
+      </Formik>
     </>
   );
 };
