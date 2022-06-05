27ebfa5b1ecd0f76efc8303fa2f94c4c0a3401bd
AlexanderDmitriev <alex9110077@gmail.com>
2022-06-05 15:40:24 +0300

    Update ContactsPage.jsx

--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -1,13 +1,35 @@
+import Typography from '@mui/material/Typography';
+import { useSelector } from 'react-redux';
+
 const ContactsPage = () => {
+  const loggedIn = useSelector(state => state.isLoggedIn);
+
   return (
     <>
       <button type="button">addNewContact button-open modal</button>
-      <p>Filter</p>
+      <Typography
+        variant="h6"
+        component="h2"
+        color="inherit"
+        sx={{ flexGrow: 1 }}
+      >
+        Filter
+      </Typography>
       <label>
         Filter input
         <input type="text" name="filter" />
       </label>
-      <p>You have no contacts yet</p>
+      {!loggedIn && (
+        <Typography
+          variant="h6"
+          component="h2"
+          color="inherit"
+          sx={{ flexGrow: 1 }}
+        >
+          You have no contacts yet
+        </Typography>
+      )}
+
       <p>Contact list</p>
       <ul>
         <li>
